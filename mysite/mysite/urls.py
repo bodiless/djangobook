1f11a2e54fe028144117327d0bf6295ff61e410a
hadrian@kalski.pl <hka@Hadrians-MacBook-Air.local>
2014-05-20 11:21:27 +0200

    next chapter - not finished

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,12 +1,20 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+
+from mysite.views import *
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+	url(r'^$', index),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$', hello),
+    url(r'^hour/(\d{1})/', one_hour_ahead),
+    url(r'^datetime/', current_datetime),
+
 )
+
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,9 +1,32 @@
-from django.http import HttpResponse
-
+from django.http import HttpResponse, Http404
+from django.template import Context
+from django.template.loader import get_template
+import datetime
 def hello(request):
 	return HttpResponse("Hello World!")
 
-def whatANight(request):
-	return HttpResponse("send me photo")
+def index(request):
+	tmplt = get_template('base.html')
+	now = get_datetime()
+	html = tmplt.render(Context({'title': 'index.html', 'current_date': now}))
+	return HttpResponse(html)
 
-def finalyy
+def one_hour_ahead(request,offset):
+	try:		
+		offset = int(offset)
+	except ValueError:
+		raise Http404()
+	now = datetime.datetime.now()
+	hour_ahead = datetime.datetime.now() + datetime.timedelta(hours=offset)
+	html = "Now is: %s <br/> In %s hours ahead will be: %s" % (now,offset,hour_ahead)
+	return HttpResponse(html)
+
+def current_datetime(request):
+	now = datetime.datetime.now()
+	tmplt = get_template('datetime.html')
+	html = tmplt.render(Context({'current_date': now}))
+	return HttpResponse(html)
+
+def get_datetime():
+	now = datetime.datetime.now()
+	return now
